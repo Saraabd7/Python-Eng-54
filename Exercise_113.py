7d86637297cbaac8a1d499a781520af9cea2a5c7
Sara Abdrabu <Sabdrabu@tcadmins-MacBook-Air.local>
2020-03-05 16:43:43 +0000

    Update

--- Exercise_113.py
+++ Exercise_113.py
@@ -5,37 +5,37 @@
 # phase 2: find area of a circle, triangle and square
 
 import math
-x = int(input('Enter your first number: '))
-y = int(input('Enter your second number: '))
 
 def cal_add(x, y):
     return x + y
-print(cal_add(x, y))
+print(cal_add(3, 5))
 def cal_subtract(x, y):
     return x - y
-print(cal_add(x, y))
+print(cal_subtract(12, 6))
 
 def cal_multiply(x, y):
     return x * y
-print(cal_add(x, y))
+print(cal_multiply(4, 2))
+
 def cal_divide(x, y):
     return x / y
-print(cal_divide )
-print(cal_add(x, y))
-
+print(cal_divide(18, 2))
 
 def triangle_area(base, height):
   return 0.5 * base * height
-print(triangle_area(x, y))
+print(triangle_area(10, 5))
+
+
+
 
 def circle_area(radius):
     return (radius ** 2) * math.pi
-print(circle_area(x))
+print(circle_area(60))
 
-def square_area(hight, length)
+
+
+def square_area(hight, length):
     return (hight * length)
-print(square_area(x * y))
-
-
+print(square_area(4 , 4))
 
 
